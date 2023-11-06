5a1d752675cd7abae62e45313ee7cd7d9dedaae5
Romanmanco <romanmanco@gmail.com>
2023-11-06 15:59:13 +0300

    refactoring code added TestStorage_UpdateURL, TestStorage_ShowAllURLs, TestStorage_DeleteURL

--- internal/storage/client/postgres/postgres_test.go
+++ internal/storage/client/postgres/postgres_test.go
@@ -13,7 +13,7 @@ const testAlias = "test_alias"
 const testURL = "http://example.com"
 
 // before test init db
-func InitDb(t *testing.T) *sql.DB {
+func initDb(t *testing.T) *sql.DB {
 	// connect to test db
 	db, err := sql.Open("postgres", dbTestURL)
 	if err != nil {
@@ -23,8 +23,21 @@ func InitDb(t *testing.T) *sql.DB {
 	return db
 }
 
+// clean table
+func cleanDb(t *testing.T, testDb *sql.DB) {
+	_, err := testDb.Exec("DELETE FROM url")
+	assert.NoError(t, err, "Failed to clear the database")
+}
+
+// save test data before checks
+func saveDataForChecks(t *testing.T, storage *Storage, testURL, testAlias string) {
+	url, err := storage.SaveURL(testURL, testAlias)
+	assert.NoError(t, err, "Failed to save URL")
+	assert.NotEmpty(t, url, "Expected not empty URL")
+}
+
 func TestNew(t *testing.T) {
-	testDb := InitDb(t)
+	testDb := initDb(t)
 
 	defer func(testDb *sql.DB) {
 		err := testDb.Close()
@@ -46,7 +59,7 @@ func TestNew(t *testing.T) {
 }
 
 func TestStorage_SaveURL(t *testing.T) {
-	testDb := InitDb(t)
+	testDb := initDb(t)
 
 	defer func(testDb *sql.DB) {
 		err := testDb.Close()
@@ -58,24 +71,21 @@ func TestStorage_SaveURL(t *testing.T) {
 	// create db obj
 	storage := &Storage{db: testDb}
 
-	// clean table
-	_, err := testDb.Exec("DELETE FROM url")
-	assert.NoError(t, err, "Failed to clear the database")
+	// clean table before ad new date
+	cleanDb(t, testDb)
 
 	// add test data to save, checking that id is positive
-	id, err := storage.SaveURL(testURL, testAlias)
-	assert.NoError(t, err, "Failed to save URL")
-	assert.NotNil(t, id, "Expected a positive ID")
+	saveDataForChecks(t, storage, testURL, testAlias)
 
 	// checking that data is correct
 	var url string
-	err = testDb.QueryRow("SELECT url FROM url WHERE alias = $1", testAlias).Scan(&url)
+	err := testDb.QueryRow("SELECT url FROM url WHERE alias = $1", testAlias).Scan(&url)
 	assert.NoError(t, err, "Failed to save URL")
 	assert.Equal(t, testURL, "http://example.com")
 }
 
 func TestStorage_GetURL(t *testing.T) {
-	testDb := InitDb(t)
+	testDb := initDb(t)
 
 	defer func(testDb *sql.DB) {
 		err := testDb.Close()
@@ -87,13 +97,11 @@ func TestStorage_GetURL(t *testing.T) {
 	// create db obj
 	storage := &Storage{db: testDb}
 
-	// clean table before checks
-	_, err := testDb.Exec("DELETE FROM url")
-	assert.NoError(t, err, "Failed to clear the database")
+	// clean table before ad new date
+	cleanDb(t, testDb)
 
 	// add test data before check that get url is work
-	_, err = storage.SaveURL(testURL, testAlias)
-	assert.NoError(t, err, "Failed to save URL")
+	saveDataForChecks(t, storage, testURL, testAlias)
 
 	// get saved url
 	url, err := storage.GetURL(testAlias)
@@ -107,13 +115,90 @@ func TestStorage_GetURL(t *testing.T) {
 }
 
 func TestStorage_DeleteURL(t *testing.T) {
+	testDb := initDb(t)
 
+	defer func(testDb *sql.DB) {
+		err := testDb.Close()
+		if err != nil {
+			t.Fatalf("Failed to close test database: %v", err)
+		}
+	}(testDb)
+
+	// create db obj
+	storage := &Storage{db: testDb}
+
+	// clean table before ad new date
+	cleanDb(t, testDb)
+
+	// add test data before check that get url is work
+	saveDataForChecks(t, storage, testURL, testAlias)
+
+	// delete saved url
+	err := storage.DeleteURL(testAlias)
+	// get success msg
+	assert.Errorf(t, err, "success deletind by alias")
+
+	// trying get url after delete
+	res, err := storage.GetURL(testAlias)
+	assert.Error(t, err, "Failed to get URL")
+	assert.Empty(t, res, "Expected empty URL")
 }
 
 func TestStorage_ShowAllURLs(t *testing.T) {
+	testDb := initDb(t)
+
+	defer func(testDb *sql.DB) {
+		err := testDb.Close()
+		if err != nil {
+			t.Fatalf("Failed to close test database: %v", err)
+		}
+	}(testDb)
+
+	// create db obj
+	storage := &Storage{db: testDb}
+
+	// clean table before ad new date
+	cleanDb(t, testDb)
+
+	// add first test data before check that get url is work
+	saveDataForChecks(t, storage, testURL, testAlias)
+
+	// add second test data before check that get url is work
+	saveDataForChecks(t, storage, testURL+"/second", testAlias+"2")
+
+	// add check that show urls is work
+	urls, err := storage.ShowAllURLs()
+	assert.NoError(t, err, "Failed to show URL")
+	assert.NotEmpty(t, urls, "Expected not empty URLs")
+	assert.Equal(t, len(urls), 2, "Expected two URLs")
 
 }
 
 func TestStorage_UpdateURL(t *testing.T) {
+	testDb := initDb(t)
 
+	defer func(testDb *sql.DB) {
+		err := testDb.Close()
+		if err != nil {
+			t.Fatalf("Failed to close test database: %v", err)
+		}
+	}(testDb)
+
+	// create db obj
+	storage := &Storage{db: testDb}
+
+	// clean table before ad new date
+	cleanDb(t, testDb)
+
+	// add test data before check that get url is work
+	saveDataForChecks(t, storage, testURL, testAlias)
+
+	// add check that update url is work
+	err := storage.UpdateURL(testAlias, testURL+"/new")
+	// get success msg
+	assert.Errorf(t, err, "success update by alias")
+
+	// trying get url after update
+	res, err := storage.GetURL(testAlias)
+	assert.Contains(t, res, "http://example.com/new")
 }
